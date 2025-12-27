9f3a0305c9eed06817cabef5101a6f3981fa8c3b
rdschrs <rdschrs@gmail.com>
2025-12-27 12:48:32 -0800

    changes from sql

--- app/database.py
+++ app/database.py
@@ -6,8 +6,11 @@ from dotenv import load_dotenv
 # 1. Load environment variables
 load_dotenv()
 
-# 2. Get the URL from the .env file
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db") # Default to SQLite if not set
+# If the URL starts with "postgres://", change it to "postgresql://" (Fix for Railway)
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
